4d39474c974befddf735d6c7e9cb83b0a0f5f1b4
Edward Hernandez <edwardhernandez@MacBook-Pro-de-Edward.local>
2020-09-23 17:48:47 -0500

    nuevo comit

--- resources/views/projects/_tabs_edit.blade.php
+++ resources/views/projects/_tabs_edit.blade.php
@@ -6,14 +6,10 @@ Actualizar proyecto
 @section('content')
 
   @include('partials.validation-errors')
-
-
   <div class="container"> 
     <div class="row">
       <div class="col-12 col-sm-10 col-lg-6 mx-auto">
         <div class="bg-white shadow rounded py-3 px-4">
-
-
 
 <ul class="nav nav-tabs" id="myTab" role="tablist">
   <li class="nav-item" role="presentation">
@@ -22,7 +18,7 @@ Actualizar proyecto
   <li class="nav-item" role="presentation">
     <a class="nav-link" id="asesor-tab" data-toggle="tab" href="#asesor" role="tab" aria-controls="asesor" aria-selected="false">Asesor</a>
   </li>
-  <li class="nav-item" role="presentation">
+  <li class="nav-item" role="">
     <a class="nav-link" id="coasesores-tab" data-toggle="tab" href="#coasesores" role="tab" aria-controls="coasesores" aria-selected="false">Coasesores</a>
   </li>
   <li class="nav-item" role="presentation">
@@ -37,14 +33,18 @@ Actualizar proyecto
   	@include('projects.edit',['btnText' => 'Actualizar','h1name' => 'Editar proyecto'])
   </div>
   <div class="tab-pane fade" id="asesor" role="tabpanel" aria-labelledby="asesor-tab">
-    @include('projects.user_list',['text' => 'asesor','role_id'=>2])
+    @include('projects.user_list',['text' => 'asesor','formulario'=>'modalAsesor','role_id'=>2])
   </div>
-  <div class="tab-pane fade" id="coasesores" role="tabpanel" aria-labelledby="coasesores-tab">@include('projects.user_list',['text' => 'coasesor','role_id'=>3])</div>
+  <div class="tab-pane fade" id="coasesores" role="tabpanel" aria-labelledby="coasesores-tab">
+    @include('projects.user_list',['text' => 'coasesor','formulario'=>'modalCoasesor','role_id'=>3])
+  </div>
   <div class="tab-pane fade" id="jurados" role="tabpanel" aria-labelledby="jurados-tab">
-    @include('projects.user_list',['text' => 'jurados','role_id'=>4])
+    @include('projects.user_list',['text' => 'jurados','formulario'=>'#modalJurado','role_id'=>4])
   </div>
-  <div class="tab-pane fade" id="estudiantes" role="tabpanel" aria-labelledby="estudiantes-tab">@include('projects.user_list',['text' => 'estudiante','role_id'=>5])</div>
-</div>
+  <div class="tab-pane fade" id="estudiantes" role="tabpanel" aria-labelledby="estudiantes-tab">
+    @include('projects.user_list',['text' => 'estudiante','formulario'=>'modalEstudiante','role_id'=>5])
+  </div>
+  
 
 
 
--- resources/views/projects/user_list.blade.php
+++ resources/views/projects/user_list.blade.php
@@ -1,14 +1,19 @@
 <br>
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+<button type="button"  id="btnmodal" class="btn btn-primary">
   Agregar {{$text}}
 </button>
+<script>
+  $("#btnmodal").on("click",function(){
+        $("{{$formulario}}").modal("show");
+    });
+</script>
 <br><br>
 <table class="table">
   <thead>
     <tr>
-      <th scope="col">Nombre</th>
-      <th scope="col">Correo</th>
+      <th scope="col">id role {{$role_id}}</th>
+      <th scope="col">formulario {{$formulario}}</th>
       <th scope="col">Acciones</th>
     </tr>
   </thead>
@@ -28,12 +33,12 @@
 </table>
 
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<!-- Modal para asesor-->
+<div class="modal fade" id="modalAsesor" tabindex="-1" role="dialog" aria-labelledby="modalAsesorLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Agregar {{$text}}</h5>
+        <h5 class="modal-title" id="modalAsesorLabel">Agregar {{$text}}</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
@@ -42,21 +47,108 @@
         
         <form method="POST" action="{{route('adminproyectos.adduser',$project)}}" id="form_user">
           @csrf
-            <label formuser>Seleccione asesor</label>
+            <label formuser>Seleccione</label>
+            <select class="form-control" name="user_id">
+              @foreach($teachers as $teacher)
+                <option value="{{$teacher->id}}">{{$teacher->name}}</option>
+              @endforeach
+            </select>
+            <input value="{{$role_id}}" type="hidden" name="role_id">
+            
+            <button form="form_user" type="submit" class="btn btn-primary btn-lg btn-block" >Agregar
+            </button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal para coasesor-->
+<div class="modal fade" id="modalCoasesor" tabindex="-1" role="dialog" aria-labelledby="modalCoasesorLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalCoasesorLabel">Agregar {{$text}}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        
+        <form method="POST" action="{{route('adminproyectos.adduser',$project)}}" id="form_user">
+          @csrf
+            <label formuser>Seleccione {{$text}}</label>
             <select class="form-control" name="user_id">
               @foreach($teachers as $teacher)
                 <option value="{{$teacher->id}}">{{$teacher->name}}</option>
               @endforeach
               <input value="{{$role_id}}" type="hidden" name="role_id">
-          
             </select>
             
             <button form="form_user" type="submit" class="btn btn-primary btn-lg btn-block" >Agregar
             </button>
-          
         </form>
       </div>
+    </div>
+  </div>
+</div>
 
+<!-- Modal para jurado-->
+<div class="modal fade" id="modalJurado" tabindex="-1" role="dialog" aria-labelledby="modalJuradoLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalJuradoLabel">Agregar {{$text}}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        
+        <form method="POST" action="{{route('adminproyectos.adduser',$project)}}" id="form_user">
+          @csrf
+            <label formuser>Seleccione {{$text}}</label>
+            <select class="form-control" name="user_id">
+              @foreach($teachers as $teacher)
+                <option value="{{$teacher->id}}">{{$teacher->name}}</option>
+              @endforeach
+              <input value="{{$role_id}}" type="hidden" name="role_id">
+            </select>
+            
+            <button form="form_user" type="submit" class="btn btn-primary btn-lg btn-block" >Agregar
+            </button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal para estudiante-->
+<div class="modal fade" id="modalEstudiante" tabindex="-1" role="dialog" aria-labelledby="modalEstudianteLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="modalEstudianteLabel">Agregar {{$text}}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        
+        <form method="POST" action="{{route('adminproyectos.adduser',$project)}}" id="form_user">
+          @csrf
+            <label formuser>Seleccione {{$text}}</label>
+            <select class="form-control" name="user_id">
+              @foreach($students as $student)
+                <option value="{{$student->id}}">{{$student->name}}</option>
+              @endforeach
+              <input value="{{$role_id}}" type="hidden" name="role_id">
+            </select>
+            
+            <button form="form_user" type="submit" class="btn btn-primary btn-lg btn-block" >Agregar
+            </button>
+        </form>
+      </div>
     </div>
   </div>
 </div>
